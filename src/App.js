dafa6adf959618ddc949f5d0ffb1f94c89dc9d15
ujjwal <ujjwalsharma1772003@gmail.com>
2023-05-31 12:36:14 +0530

    added expense.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import Expenseitem from "./components/Expenseitem";
 
+import Expense from "./components/expense";
 function App() {
   const expenses = [
     {
@@ -23,24 +23,7 @@ function App() {
     },
   ];
   return (
-    <div>
-      <h2>Let's get started!</h2>
-     <Expenseitem
-     title={expenses[0].title}
-     amount = {expenses[0].amount}
-     date={expenses[0].date}></Expenseitem>
-     <Expenseitem  title={expenses[1].title}
-     amount = {expenses[1].amount}
-     date={expenses[1].date}></Expenseitem>
-     <Expenseitem
-      title={expenses[2].title}
-      amount = {expenses[2].amount}
-      date={expenses[2].date}></Expenseitem>
-     <Expenseitem
-      title={expenses[3].title}
-     amount = {expenses[3].amount}
-     date={expenses[3].date}></Expenseitem>
-    </div>
+   <Expense items = {expenses}></Expense>
   );
 }
 
---
+++ src/components/expense.js
@@ -0,0 +1,26 @@
+import Expenseitem from './Expenseitem'
+
+function Expense(props) {
+
+    return (
+        <div>
+        <Expenseitem
+            title={props.items[0].title}
+            amount={props.items[0].amount}
+            date={props.items[0].date}></Expenseitem>
+        <Expenseitem title={props.items[1].title}
+            amount={props.items[1].amount}
+            date={props.items[1].date}></Expenseitem>
+        <Expenseitem
+            title={props.items[2].title}
+            amount={props.items[2].amount}
+            date={props.items[2].date}></Expenseitem>
+        <Expenseitem
+            title={props.items[3].title}
+            amount={props.items[3].amount}
+            date={props.items[3].date}></Expenseitem>
+   </div>
+    )
+}
+
+export default Expense;
