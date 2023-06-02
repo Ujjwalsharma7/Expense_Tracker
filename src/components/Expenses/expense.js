accc3213ad365ac152411a3496dfd9ebaa5ed2e4
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-02 14:58:41 +0530

    added to expense.js

--- src/components/Expenses/expense.js
+++ src/components/Expenses/expense.js
@@ -1,11 +1,19 @@
 import Expenseitem from './Expenseitem'
 import './expense.css'
 import Card from '../UI/Card';
+import ExpensesFilter from './Expensefilter';
+import { useState } from 'react';
 
 function Expense(props) {
+  const [filterYear, setFilteryear] = useState('2022')
+    const filterChangeHandler = Selectedyear =>{
+       setFilteryear(Selectedyear)
+    }
 
     return (
+        <div>
         <Card className='expense'>
+            <ExpensesFilter selected = {filterYear} onFilterChnage = {filterChangeHandler}/>
         <Expenseitem
             title={props.items[0].title}
             amount={props.items[0].amount}
@@ -22,6 +30,7 @@ function Expense(props) {
             amount={props.items[3].amount}
             date={props.items[3].date}></Expenseitem>
    </Card>
+   </div>
     )
 }
 
