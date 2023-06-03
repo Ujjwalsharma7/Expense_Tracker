d76a6de91b86c490e44f82dc700bac0cab11c32b
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-03 16:18:10 +0530

    Dynamic array

--- src/components/Expenses/expense.js
+++ src/components/Expenses/expense.js
@@ -14,21 +14,14 @@ function Expense(props) {
         <div>
         <Card className='expense'>
             <ExpensesFilter selected = {filterYear} onFilterChnage = {filterChangeHandler}/>
-        <Expenseitem
-            title={props.items[0].title}
-            amount={props.items[0].amount}
-            date={props.items[0].date}></Expenseitem>
-        <Expenseitem title={props.items[1].title}
-            amount={props.items[1].amount}
-            date={props.items[1].date}></Expenseitem>
-        <Expenseitem
-            title={props.items[2].title}
-            amount={props.items[2].amount}
-            date={props.items[2].date}></Expenseitem>
-        <Expenseitem
-            title={props.items[3].title}
-            amount={props.items[3].amount}
-            date={props.items[3].date}></Expenseitem>
+            {props.items.map(expense => <Expenseitem 
+            title = {expense.title} 
+            amount = {expense.amount} 
+            date={expense.date}
+            />
+            )}
+        
+        
    </Card>
    </div>
     )
