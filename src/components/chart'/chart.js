c5f0cb43e6cd3cda9d4b102273eba403c5f580a4
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-05 12:11:42 +0530

    Added ExpensesChart.js

--- src/components/chart'/chart.js
+++ src/components/chart'/chart.js
@@ -2,10 +2,13 @@
 import ChartBar from "./chartBar";
 import './chart.js'
 const Chart = (props) =>{
+    const datapointValues = props.datapoints.map(datapoint => datapoint.value)
+    const totalMaximum = Math.max(...datapointValues);
 return (
 <div>
     {props.datapoints.map((datapoint) => ( 
-    <ChartBar 
+    <ChartBar
+    key={datapoint.label} 
     value = {datapoint.value} 
     maxValue={null} 
     label = {datapoint.label}
