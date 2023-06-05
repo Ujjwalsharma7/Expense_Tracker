1677bcbd8b5052a42fa0b519d97225249b98ac83
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-05 11:18:41 +0530

    Hiding form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,20 +1,35 @@
 import React from 'react';
+import { useState } from 'react';
 
 import ExpenseForm from './Expenseform';
 import './NewExpense.css';
 
 const NewExpense = (props) => {
+const [isEditing, setisEditing] = useState(false)
+
   const saveExpenseDataHandler = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
       id: Math.random().toString()
     };
     props.onAddExpense(expenseData);
+    setisEditing(false);
   };
+
+  const startEditingHnadler = () =>{
+    setisEditing(true);
+  }
+  const stopEditingHandler = () =>{
+    setisEditing(false)
+  }
 
   return (
     <div className='new-expense'>
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+     {!isEditing && <button onClick={startEditingHnadler}>Add New Expense</button>}
+      {isEditing && <ExpenseForm
+       onSaveExpenseData={saveExpenseDataHandler}
+       onCancel = {stopEditingHandler}
+       />}
     </div>
   );
 };
