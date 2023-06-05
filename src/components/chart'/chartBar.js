a629770327cb5df1819a7e95346f42bfdb1ef1cd
ujjwal <ujjwalsharma1772003@gmail.com>
2023-06-05 11:44:22 +0530

    added chartbar.js

---
+++ src/components/chart'/chartBar.js
@@ -0,0 +1,13 @@
+import './chartBar.css'
+
+const ChartBar = (props) => {
+return (
+<div className="chart-bar">
+    <div className="chart-bat__inner">
+        <div className="chart-bar__fill"></div>
+    </div>
+    <div className="chart-bar__label"></div>
+</div>
+)
+}
+export default ChartBar;
